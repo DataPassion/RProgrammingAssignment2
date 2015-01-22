8dd6a33e84fab13163e3347299eb467267d6ff7e
Nikhil_Bangad <nikhilbangad@gmail.com>
2015-01-21 22:41:22 -0500

    Modification to the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,41 +1,71 @@
-## Name:- 						        Nikhil Bangad
-## Subject :- 					      R Programming (Coursera)
-## Assignment:- 				      Assignment 2 : Caching the Inverse of a Matrix
-## Assignment Description:- 	Matrix inversion is usually a costly computation and their may be some benefit to caching the inverse of a matrix rather than compute it repeatedly. 
-##                          	One of the technique is demonstrated below
-## Date:-						          01/21/2015
+## Name:-   			Nikhil Bangad                                       #
+## Subject :- 		R Programming (Coursera)                            #
+## Assignment:- 	Assignment 2 : Caching the Inverse of a Matrix      #
+## Date:-					01/21/2015                                          #
+#######################################################################
 
-
-
-## This function generates a matrix that can cache its inverse
+#################################################################
+## This function generates a matrix that can cache its inverse. #
+#################################################################
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  #Variable m is declared uniquely and is allocated separate addresses in memory
   m<-NULL
-  set<-function(y){
-    x<<-y
+  
+  set<-function(y)
+  {
+    x<<-y   ## <<- is a 'super-assignment' operator
     m<<-NULL
   }
+  
   get<-function() x
+  
   setmatrix<-function(solve) m<<- solve
+  
   getinverse<-function() m
+  
   list(set=set, get=get,
        setmatrix=setmatrix,
        getinverse=getinverse)
 }
 
+###############################################################################################################
+## Notes for better understanding                                                                             #
+## In function makeCacheMatrix() you’ll notice variable m is declared immediately and assigned the value NULL #  
+## using the standard assignment operator (<-).                                                               # 
+## However, the “set” functions defined within the containing makeCacheMatrix() function require the special  #
+## assignment operator (<–) to update the value of variable m; it is important to remember variable m was     #
+## declared and initialised by makeCacheMatrix().                                                             #
+## Had functions set() and setmatrix() not used the special assignment operator, these functions would        #
+## have allocated memory to store the value and labelled the address as m.                                    #  
+## The variables named m would effectively be isolated and distinct variables.                                #
+###############################################################################################################
 
-## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+
+##################################################################################################
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. #
+## If the inverse has already been calculated (and the matrix has not changed), then the         #
+## cachesolve should retrieve the inverse from the cache.                                        #
+##################################################################################################
 
 cacheSolve <- function(x, ...) {
+  
   ## Return a matrix that is the inverse of 'x'
+  #Variable m is declared uniquely and is allocated separate addresses in memory
   m<-x$getinverse()
-  if(!is.null(m)){
+  
+  if(!is.null(m))
+  {
     message("getting cached data")
     return(m)
   }
+  
   matrix<-x$get()
+  
   m<-solve(matrix, ...)
+  
   x$setmatrix(m)
-  m
+  
+  m  # Return the matrix
 }
